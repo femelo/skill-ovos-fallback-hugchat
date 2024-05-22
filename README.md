d9f7f820d35ede5b92d78a708fb67744866fc4cd
Flávio Eler De Melo <flavio.eler@gmail.com>
2024-05-23 01:00:16 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# HuggingChat Fallback Skill
+
+When in doubt, ask HuggingChat, powered by [HuggingChat Solver](https://github.com/femelo/ovos-solver-plugin-hugchat-persona).
+
+You need to configure a `email` and `password` for your [Hugging Face](https://huggingface.co) account.
+
+## About
+
+Capabilities:
+
+- Remembers what user said earlier in the conversation
+- Can augment the answer with web content
+- Trained to decline inappropriate requests
+
+Limitations:
+
+- May occasionally generate incorrect information
+- May occasionally produce harmful instructions or biased content
+
+## Configuration
+
+Under skill settings (`.config/mycroft/skills/skill-ovos-fallback-hugchat.openvoiceos/settings.json`) you can tweak some parameters for HuggingChat.
+
+| Option          | Value                                                                  | Description                             |
+| --------------- | ---------------------------------------------------------------------- | --------------------------------------- |
+| `email`         | `your-hf-email`                                                        | Your `email` to access Hugging Face     |
+| `password`      | `your-hf-password`                                                     | Your `password` to access Hugging Face  |
+| `persona`       | `You are a helpful assistant who gives very short but factual answers` | Give a personality to HuggingChat       |
+| `model`         | `llama-3`                                                              | LLM model to use                        |
+| `enable_memory` | `true`                                                                 | Remember the last generated outputs     |
+| `memory_size`   | `15`                                                                   | How many interactions to keep           |
+| `web_search`    | `false`                                                                | Allow assistant to search the web       |
+| `name`          | `AI assistant`                                                         | Name to give to the AI assistant        |
+| `confirmation`  | `true`                                                                 | Spoken confirmation                     |
+
+Read more about it in the OVOS technical manual, page [persona server](https://openvoiceos.github.io/ovos-technical-manual/persona_server/#compatible-projects)
+
+The default persona is `You are a helpful voice assistant with a friendly tone and fun sense of humor. You respond in 40 words or fewer`
+
+## Configurations
+
+The skill utilizes the `~/.config/mycroft/skills/skill-ovos-fallback-hugchat.openvoiceos/settings.json` file which allows you to configure it.
+
+### Configuration for use with HuggingChat
+
+```json
+{
+  "email": "{your-hf-email}",
+  "password": "{your-hf-password}",
+  "model": "llama-3",
+  "persona": "You are a helpful voice assistant with a friendly tone and fun sense of humor",
+  "enable_memory": true,
+  "memory_size": 15,
+  "web_search": false,
+  "__mycroft_skill_firstrun": false
+}
+```
+
+## Examples
+
+- "Explain quantum computing in simple terms"
+- "Got any creative ideas for a 10 year old’s birthday?"
---
+++ __init__.py
@@ -0,0 +1,117 @@
+from ovos_bus_client.session import SessionManager
+from ovos_solver_hugchat_persona import HuggingChatPersonaSolver
+from ovos_utils import classproperty
+from ovos_utils.process_utils import RuntimeRequirements
+from ovos_workshop.skills.fallback import FallbackSkill
+
+
+DEFAULT_SETTINGS = {
+    "persona": "You are a helpful voice assistant with a friendly tone and fun sense of humor. You respond in 40 words or fewer.",
+    "model": "llama-3",
+}
+
+
+class HuggingChatSkill(FallbackSkill):
+    sessions = {}
+
+    @classproperty
+    def runtime_requirements(self):
+        return RuntimeRequirements(
+            internet_before_load=True,
+            network_before_load=True,
+            requires_internet=True,
+            requires_network=True,
+        )
+
+    def initialize(self):
+        self.settings.merge(DEFAULT_SETTINGS, new_only=True)
+        self.add_event("speak", self.handle_speak)
+        self.add_event("recognizer_loop:utterance", self.handle_utterance)
+        self.register_fallback(self.ask_hugchat, 85)
+
+    @property
+    def ai_name(self):
+        return self.settings.get("name", "Hugging Chat")
+
+    @property
+    def confirmation(self):
+        return self.settings.get("confirmation", True)
+
+    @property
+    def chat(self):
+        """created fresh to allow key/url rotation when settings.json is edited"""
+        try:
+            return HuggingChatPersonaSolver(config=self.settings)
+        except Exception as err:
+            self.log.error(err)
+            return None
+
+    def handle_utterance(self, message):
+        utt = message.data.get("utterances")[0]
+        sess = SessionManager.get(message)
+        if sess.session_id not in self.sessions:
+            self.sessions[sess.session_id] = []
+        self.sessions[sess.session_id].append(("user", utt))
+
+    def handle_speak(self, message):
+        utt = message.data.get("utterance")
+        sess = SessionManager.get(message)
+        if sess.session_id in self.sessions:
+            self.sessions[sess.session_id].append(("ai", utt))
+
+    def build_msg_history(self, message):
+        sess = SessionManager.get(message)
+        if sess.session_id not in self.sessions:
+            return []
+        messages = []  # tuple of question, answer
+
+        q = None
+        ans = None
+        for m in self.sessions[sess.session_id]:
+            if m[0] == "user":
+                q = m[1]  # track question
+                if ans is not None:
+                    # save previous q/a pair
+                    messages.append((q, ans))
+                    q = None
+                ans = None
+            elif m[0] == "ai":
+                if ans is None:
+                    ans = m[1]  # track answer
+                else:  # merge multi speak answers
+                    ans = f"{ans}. {m[1]}"
+
+        # save last q/a pair
+        if ans is not None and q is not None:
+            messages.append((q, ans))
+        return messages
+
+    def _async_ask(self, message):
+        utterance = message.data["utterance"]
+        self.chat.qa_pairs = self.build_msg_history(message)
+        answered = False
+        try:
+            for utt in self.chat.stream_utterances(utterance):
+                answered = True
+                self.speak(utt)
+        except Exception as err:  # speak error on any network issue / no credits etc
+            self.log.error(err)
+        if not answered:
+            self.speak_dialog("gpt_error", data={"name": self.ai_name})
+
+    def ask_hugchat(self, message):
+        if "key" not in self.settings:
+            self.log.error(
+                "HuggingChat not configured yet, please set your API key in %s",
+                self.settings.path,
+            )
+            return False  # HuggingChat not configured yet
+        utterance = message.data["utterance"]
+        if self.confirmation:
+            self.speak_dialog("asking", data={"name": self.ai_name})
+        # ask in a thread so fallback doesnt timeout
+        self.bus.once("async.hugchat.fallback", self._async_ask)
+        self.bus.emit(
+            message.forward("async.hugchat.fallback", {"utterance": utterance})
+        )
+        return True
